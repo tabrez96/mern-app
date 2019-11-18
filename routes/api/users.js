bc3466c0a656c702a2d40cfc1ae9f6b9b48a0b63
Tabrez Basha <tab22rez@gmail.com>
2019-11-18 23:29:30 +0530

    Provision to generate jwt token and send it to client

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -35,7 +37,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.user(400).json({
+        return res.status(400).json({
           errors: [{
             msg: 'User already exists'
           }]
@@ -61,7 +63,23 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
+
     } catch(err) {
       console.log(err.message);
       res.status(500).send('Server error');
